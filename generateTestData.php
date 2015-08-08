87b517b1d2e82f06fced9baca76053ba525bcd44
Yousan_O <yousan@l2tp.org>
2015-08-08 19:38:18 +0900

    add test data

--- generateTestData.php
+++ generateTestData.php
@@ -11,13 +11,13 @@ $generateTestData->generate();
 
 class GenerateTestData {
 
-    const NW_LONGITUDE = 37.5264602; // north west 北西
-    const NW_LATITUDE = 139.8918034; // north west 北西
+    const NW_LATITUDE = 37.5264602; // north west 北西
+    const NW_LONGITUDE = 139.8918034; // north west 北西
 
-    const SE_LONGITUDE = 37.4832233; // north west 南東
-    const SE_LATITUDE = 139.9542452; // north west 南東
+    const SE_LATITUDE = 37.4832233; // north west 南東
+    const SE_LONGITUDE = 139.9542452; // north west 南東
 
-    const MAX_POINT = 1;
+    const MAX_POINT = 50000;
 
     const Dividing = 5;
 
@@ -32,30 +32,32 @@ class GenerateTestData {
 
 
     private function getWidth() {
-        return self::NW_LATITUDE - self::SE_LATITUDE;
+        return self::NW_LONGITUDE - self::SE_LONGITUDE;
     }
 
     private function getHeight() {
-        return self::NW_LONGITUDE - self::SE_LONGITUDE;
+        return self::NW_LATITUDE - self::SE_LATITUDE;
     }
 
     public function __construct() {
         $this->NW_point = new Geopoint(self::NW_LONGITUDE, self::NW_LATITUDE);
         $this->SE_point = new Geopoint(self::SE_LONGITUDE, self::SE_LATITUDE);
         $this->generate5x5Juu();
+        ///var_dump($this->juus);exit;
     }
 
     public function generate() {
-        $sasukeneRow = new SasukeneRow();
-        $sasukeneRow->status = 0;
-        $sasukeneRow->status = 1;
         $rets = array();
         for($i=0; $i<self::MAX_POINT; $i++) {
+            $sasukeneRow = new SasukeneRow();
+            $sasukeneRow->status = 0;
+            $sasukeneRow->type = 1;
             $latitude = rand(0, self::Dividing-1);
             $longitutude = rand(0, self::Dividing-1);
             $point = $this->juus[$latitude][$longitutude];
             $sasukeneRow->geo = $point;
             $rets['results'][] = $sasukeneRow;
+            unset($sasukeneRow);
         }
         echo json_encode($rets);
     }
@@ -79,17 +81,18 @@ class GenerateTestData {
 }
 
 class SasukeneRow {
+    public $name = 'hoge';
     public $geo;
-    public $type;
-    public $status;
+//    public $type;
+//    public $status;
 }
 
 
 class GeoPoint {
+    public $__type = 'GeoPoint';
     public $longitude; // 経度
     public $latitude; // 緯度 139
 
-    public $__type = 'GeoPoint';
 
     public function __construct($longitude, $latitude) {
         $this->longitude = $longitude;
