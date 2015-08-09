3e5d18502d7be5b8b4d6e163d66b7bc6f6abfd01
Yousan_O <yousan@l2tp.org>
2015-08-09 10:07:42 +0900

    adding overlay image. this will be controlled by js.

--- imageonimage.js
+++ imageonimage.js
@@ -1,4 +1,18 @@
-$("#main_map").each(function() {
+/**    $(function(){
+       $overlay.css({
+            position: "relative",
+            display: "inline-block",
+            width: $(this).width(),
+            height: $(this).height(),
+            backgroundPosition: "center center",
+            backgroundImage: "url(yellow100.PNG)"
+        });
+        $('#main_map').wrap($overlay);
+    });
+**/
+
+
+$("#main_maphoge").each(function() {
     var $overlay = $('<div></div>');
     $overlay.css({
         position: "relative",
